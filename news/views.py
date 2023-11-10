7e76e737cd5c6e4e46adf658e9e5fc77474383d9
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-11-09 21:13:34 -0300

    fix: details id error

--- news/views.py
+++ news/views.py
@@ -9,7 +9,7 @@ def index(request):
   noticias = Noticia.objects.all()
   return render(request, 'index.html', {'noticias': noticias})
 
-def detalhes(request):
+def detalhes(request, id):
   noticia = Noticia.objects.get(pk=id)
   return render(request,
                 'detalhes.html',
